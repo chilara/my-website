6109a2b69735efb79bd4c0b4a76d088f129bbf35
Chisom Timoty Oguine <oguinechisomtimothy@gmail.com>
2022-06-14 11:56:44 -0700

    fixed toggler bug

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function toggler() {
-    const menuMobile = document.querySelector(".menu-mobile");
-    if (menuMobile.className.includes("hide")) {
-      menuMobile.className = "menu-mobile show";
-    } else {
-      menuMobile.className = "menu-mobile hide";
-    }
-  }
+
   
+
+  const menuMobile = document.querySelector(".menu-mobile");
+function toggler() {
+  if (menuMobile.className.includes("hide")) {
+    menuMobile.className = "menu-mobile show";
+  } else {
+    menuMobile.className = "menu-mobile hide";
+  }
+}
+
+function resizeHandler() {
+  if (window.innerWidth > 802) {
+    menuMobile.className = "menu-mobile hide";
+  }
+}
+
+window.onresize = resizeHandler;
   
